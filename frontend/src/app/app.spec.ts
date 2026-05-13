8d4781af82f2a8853ea7fe85486a2eb67d97b74f
Edward <edwardallan.au@gmail.com>
2026-05-13 16:31:34 +1000

    wire up AppComponent shell and app config

---
+++ frontend/src/app/app.spec.ts
@@ -0,0 +1,19 @@
+import { TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { App } from './app';
+
+describe('App', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [App],
+      providers: [provideHttpClient(), provideHttpClientTesting(), provideAnimationsAsync()],
+    }).compileComponents();
+  });
+
+  it('creates the app', () => {
+    const fixture = TestBed.createComponent(App);
+    expect(fixture.componentInstance).toBeTruthy();
+  });
+});
---
+++ frontend/src/app/app.ts
@@ -0,0 +1,19 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { TodoService } from './core/services/todo.service';
+import { AddTodo } from './features/todos/add-todo/add-todo';
+import { TodoList } from './features/todos/todo-list/todo-list';
+
+@Component({
+  selector: 'app-root',
+  imports: [MatToolbarModule, AddTodo, TodoList],
+  templateUrl: './app.html',
+  styleUrl: './app.scss',
+})
+export class App implements OnInit {
+  private readonly todoService = inject(TodoService);
+
+  ngOnInit(): void {
+    this.todoService.load();
+  }
+}
